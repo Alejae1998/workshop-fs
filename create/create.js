cac85681af4e83d0d5ae2234f352de7f5e2d7a35
Alejae1998 <alejagr1998@gmail.com>
2022-12-01 10:24:12 -0800

    no hitting my supabase

--- create/create.js
+++ create/create.js
@@ -1,10 +1,11 @@
-import { getWorkshops, createParticipant } from '../fetch-utils.js';
+import { getWorkshops, createParticipant, checkAuth } from '../fetch-utils.js';
 import { renderOption } from '../render-utils.js';
 
 const workshopSelect = document.getElementById('workshops');
 const form = document.getElementById('new-participant');
 const addBtn = document.getElementById('back');
 
+checkAuth();
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
     const formData = new FormData(form);
@@ -14,9 +15,17 @@ form.addEventListener('submit', async (e) => {
         contact_info: formData.get('contact'),
         workshop_id: formData.get('workshop_id'),
     });
-    window.location.href = '/workshops';
+    window.location.replace = '/workshops';
 });
+async function onLoad() {
+    const workshops = await getWorkshops();
+    for (let workshop of workshops) {
+        const workshopOpt = renderOption(workshop);
+        workshopSelect.append(workshopOpt);
+    }
+}
+onLoad();
 
-addBtn.addEventListener('click', () => {
-    window.location.href = '/workshops';
-});
+// addBtn.addEventListener('click', () => {
+//     window.location.href = '/workshops';
+// });
