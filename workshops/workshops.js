458b8956354fd99420bb06b23b29908399facdbe
Alejae1998 <alejagr1998@gmail.com>
2022-11-30 18:11:06 -0800

    adding css to the workshop page

--- workshops/workshops.js
+++ workshops/workshops.js
@@ -1,7 +1,10 @@
 /* Imports */
-import { checkAuth, signOutUser } from '../fetch-utils.js';
+import { checkAuth, deleteParticipant, getWorkshops, signOutUser } from '../fetch-utils.js';
+import { renderWorkshop } from '../render-utils.js';
 // this will check if we have a user and set signout link if it exists
 import '../auth/user.js';
+
+checkAuth();
 
 /* Get DOM Elements */
 
@@ -10,3 +13,29 @@ import '../auth/user.js';
 /* Events */
 
 /* Display Functions */
+async function displayWorkshops() {
+    const main = document.querySelector('main');
+    main.textContent = '';
+    const data = await getWorkshops();
+    for (let workshop of data) {
+        const shopEl = renderWorkshop(workshop);
+        const ul = document.createElement('ul');
+        for (let participant of workshop.participants) {
+            const li = document.createElement('li');
+            li.textContent = `${participant.name}: ${participant.contact_info}`;
+            li.addEventListener('click', async () => {
+                await deleteParticipant(participant.id);
+                displayWorkshops();
+            });
+            ul.append(li);
+        }
+        shopEl.append(ul);
+        main.append(shopEl);
+    }
+}
+displayWorkshops();
+
+window.addEventListener('load', async () => {
+    const shops = await getWorkshops();
+    displayWorkshops(shops);
+});
